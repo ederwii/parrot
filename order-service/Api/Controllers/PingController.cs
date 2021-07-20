c40f0fec9a1f9f99fea9c51143fbbdcc58b2fd31
Alan Martinez <alan@codiks.com>
2021-07-20 15:01:43 -0500

    Added order service net 5 docker container

---
+++ order-service/Api/Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Pong");
+        }
+    }
+}
