973f881adb16edb3b6c0903e32f872ea72e1b2fe
Alan Martinez <alan@codiks.com>
2021-07-20 14:52:33 -0500

    Added user service

---
+++ user-service/index.js
@@ -0,0 +1,28 @@
+/* const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}) */
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
