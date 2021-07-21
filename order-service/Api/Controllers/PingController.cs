f02bc5aa96d33f4c50ae13b4b53b7cd578f406e6
Alan Martinez <eder.marquez@filmtrack.net>
2021-07-21 00:29:23 -0500

    Added custom jwt middleware, sorry no validation :(

--- order-service/Api/Controllers/PingController.cs
+++ order-service/Api/Controllers/PingController.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Linq;
+using Api.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class PingController : ControllerBase
     {
-
         [HttpGet]
         public IActionResult Get()
         {
